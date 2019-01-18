2d1d0b94203a3c3b25f2309050e2db6c7e84580f
Jules Aubel <jules.aubel@protonmail.com>
2019-01-18 11:15:39 +0100

    Init commit

---
+++ desktop/src/com/tmags/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tmags.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tmags.game.TooMuchAGoodSpin;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TooMuchAGoodSpin(), config);
+	}
+}
