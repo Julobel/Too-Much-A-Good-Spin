4380101b471abcc0a1c0aec8e17ff1ca188e8ae2
Jules Aubel <jules.aubel@protonmail.com>
2019-06-18 10:03:19 +0200

    feat(managers): ajout gestion état du jeu + test gestion des bind touches

--- core/src/com/tmags/game/Managers/GameInputProcessor.java
+++ core/src/com/tmags/game/Managers/GameInputProcessor.java
@@ -1,7 +1,72 @@
-package com.tmags.game.Managers;
-
 /**
  * Created by Jules Aubel on 17/06/19.
  */
-public class GameInputProcessor {
+
+package com.tmags.game.Managers;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+
+public class GameInputProcessor extends InputAdapter {
+
+    public boolean KeyDown(int k) {
+        System.out.println(k);
+        switch(k) {
+            case Input.Keys.UP:
+                GameKeys.setKey(GameKeys.UP, true);
+                break;
+            case Input.Keys.DOWN:
+                GameKeys.setKey(GameKeys.DOWN, true);
+                break;
+            case Input.Keys.LEFT:
+                GameKeys.setKey(GameKeys.LEFT, true);
+                break;
+            case Input.Keys.RIGHT:
+                GameKeys.setKey(GameKeys.RIGHT, true);
+                break;
+            case Input.Keys.ESCAPE:
+                GameKeys.setKey(GameKeys.ESCAPE, true);
+                break;
+            case Input.Keys.SPACE:
+                GameKeys.setKey(GameKeys.SPACE, true);
+                break;
+            case Input.Keys.ENTER:
+                GameKeys.setKey(GameKeys.ENTER, true);
+                break;
+            case Input.Keys.SHIFT_LEFT:
+                GameKeys.setKey(GameKeys.SHIFT, true);
+                break;
+        }
+        return true;
+    }
+
+    public boolean KeyUp(int k) {
+        switch(k) {
+            case Input.Keys.UP:
+                GameKeys.setKey(GameKeys.UP, false);
+                break;
+            case Input.Keys.DOWN:
+                GameKeys.setKey(GameKeys.DOWN, false);
+                break;
+            case Input.Keys.LEFT:
+                GameKeys.setKey(GameKeys.LEFT, false);
+                break;
+            case Input.Keys.RIGHT:
+                GameKeys.setKey(GameKeys.RIGHT, false);
+                break;
+            case Input.Keys.ESCAPE:
+                GameKeys.setKey(GameKeys.ESCAPE, false);
+                break;
+            case Input.Keys.SPACE:
+                GameKeys.setKey(GameKeys.SPACE, false);
+                break;
+            case Input.Keys.ENTER:
+                GameKeys.setKey(GameKeys.ENTER, false);
+                break;
+            case Input.Keys.SHIFT_LEFT:
+                GameKeys.setKey(GameKeys.SHIFT, false);
+                break;
+        }
+        return true;
+    }
 }
--- core/src/com/tmags/game/Managers/GameStateManager.java
+++ core/src/com/tmags/game/Managers/GameStateManager.java
@@ -6,21 +6,47 @@ package com.tmags.game.Managers;
 
 
 import com.tmags.game.GameStates.GameState;
+import com.tmags.game.GameStates.MenuState;
+import com.tmags.game.GameStates.PlayState;
 
 public class GameStateManager {
 
-    // Etat en cours du jeu
     private GameState gameState;
-
     public static final int MENU = 0;
     public static final int PLAY = 1;
-
-
+    public static final int SCORE = 2;
     public GameStateManager() {
-
+        setGameState(MENU);
     }
 
+    /**
+     * Gestion état du jeu
+     * @param state
+     */
     public void setGameState(int state) {
+        if(gameState != null) gameState.dispose();
+        switch(state) {
+            case MENU:
+                gameState = new MenuState(this);
+                break;
+            case PLAY:
+                gameState = new PlayState(this);
+                break;
+            case SCORE:
+                // TODO Gestion top 10 meilleurs scores
+                // gameState = new ScoreState(this);
+                break;
+            default:
+                gameState = new MenuState(this);
+                break;
+        }
+    }
 
+    public void update(float dt) {
+        gameState.update(dt);
+    }
+
+    public void draw() {
+        gameState.draw();
     }
 }
