234abea47bf0d7d5cd9d542cb0d41b1e780157b1
Jules Aubel <jules.aubel@protonmail.com>
2019-06-18 10:04:00 +0200

    feat(gamestates): ajout des états du jeu

--- core/src/com/tmags/game/GameStates/MenuState.java
+++ core/src/com/tmags/game/GameStates/MenuState.java
@@ -4,7 +4,143 @@
 
 package com.tmags.game.GameStates;
 
+import com.badlogic.gdx.Gdx;
 
-public class MenuState {
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.tmags.game.Main;
+import com.tmags.game.Managers.GameStateManager;
 
+public class MenuState extends GameState{
+
+    private SpriteBatch sb;
+    private BitmapFont titleFont;
+    private BitmapFont font;
+    private final String title = "Too much a good spin";
+    private int currentItem;
+    private String[] menuItems;
+    private static GlyphLayout glyphLayout = new GlyphLayout();
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        init();
+    }
+
+    @Override
+    public void init() {
+        sb = new SpriteBatch();
+        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(
+                Gdx.files.internal("fonts/nervous/Nervous.ttf")
+        );
+
+        FreeTypeFontGenerator.FreeTypeFontParameter parameterTitleFont = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameterTitleFont.size = 56;
+        parameterTitleFont.magFilter = Texture.TextureFilter.Linear;
+        parameterTitleFont.minFilter = Texture.TextureFilter.Linear;
+        parameterTitleFont.color = new com.badlogic.gdx.graphics.Color(com.badlogic.gdx.graphics.Color.WHITE);
+
+        titleFont = gen.generateFont(parameterTitleFont);
+
+        FreeTypeFontGenerator.FreeTypeFontParameter parameterFont = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameterFont.size = 40;
+        parameterFont.magFilter = Texture.TextureFilter.Linear;
+        parameterFont.minFilter = Texture.TextureFilter.Linear;
+        parameterFont.color = new com.badlogic.gdx.graphics.Color(com.badlogic.gdx.graphics.Color.WHITE);
+
+        font = gen.generateFont(parameterFont);
+
+        glyphLayout.setText(font, title);
+
+        menuItems = new String[] {
+                "Jouer",
+                "Scores",
+                "Quitter"
+        };
+    }
+
+    @Override
+    public void update(float dt) {
+        this.handleInput();
+    }
+
+    /**
+     * Affichage menu
+     */
+    @Override
+    public void draw() {
+        sb.setProjectionMatrix(Main.cam.combined);
+        sb.begin();
+
+        // Affichage titre
+        titleFont.draw(
+                sb,
+                title,
+                300,
+                550
+        );
+
+        // Affichage menu
+        for (int i = 0; i < menuItems.length; i++) {
+            if (currentItem == i) {
+                font.setColor(Color.RED);
+            } else {
+                font.setColor(Color.WHITE);
+            }
+            font.draw(
+                    sb,
+                    menuItems[i],
+                    560,
+                    370 - 80 * i
+            );
+        }
+
+        sb.end();
+
+    }
+
+    /**
+     * Gestion déplacement menu
+     */
+    @Override
+    public void handleInput() {
+        System.out.println(currentItem);
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            System.out.println("UP");
+            if(currentItem > 0) {
+                currentItem--;
+            }
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            System.out.println("DOWN");
+            if(currentItem < menuItems.length - 1) {
+                currentItem++;
+            }
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+            System.out.println("ENTER");
+            select();
+        }
+    }
+
+    /**
+     * Déclanchement action menu
+     */
+    private void select() {
+        if(currentItem == 0) {
+            gsm.setGameState(GameStateManager.PLAY);
+        } else if(currentItem == 1) {
+            // MENU SCORE
+        } else if(currentItem == 2) {
+            Gdx.app.exit();
+        }
+    }
+
+
+    @Override
+    public void dispose() {
+
+    }
 }
--- core/src/com/tmags/game/GameStates/PlayState.java
+++ core/src/com/tmags/game/GameStates/PlayState.java
@@ -1,7 +1,66 @@
-package com.tmags.game.GameStates;
-
 /**
  * Created by Jules Aubel on 17/06/19.
  */
-public class PlayState {
+
+package com.tmags.game.GameStates;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.tmags.game.Main;
+import com.tmags.game.Managers.GameKeys;
+import com.tmags.game.Managers.GameStateManager;
+import com.tmags.game.Screens.PlayScreen;
+import com.tmags.game.TooMuchAGoodSpin;
+
+public class PlayState extends GameState {
+
+    private ShapeRenderer sr;
+    private TooMuchAGoodSpin game;
+    public static int WIDTH;
+    public static int HEIGHT;
+    public static OrthographicCamera cam;
+    public SpriteBatch batch;
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+        init();
+    }
+
+    @Override
+    public void init() {
+        System.out.println("OUILLE V2");
+
+        WIDTH = Gdx.graphics.getWidth();
+        HEIGHT = Gdx.graphics.getHeight();
+
+        cam = new OrthographicCamera(WIDTH, HEIGHT);
+        cam.translate(WIDTH/2, HEIGHT/2);
+        cam.update();
+
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
 }
